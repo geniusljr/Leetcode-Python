ca9d3f79abf172a89c616fb5f1f58d3ead85a1c6
Jiarui <geniusljr@gmail.com>
2014-02-26 09:48:01 -0800

    modified version of length f last word

--- Length of Last Word.py
+++ Length of Last Word.py
@@ -2,13 +2,8 @@ class Solution:
     # @param s, a string
     # @return an integer
     def lengthOfLastWord(self, s):
-        i = len(s)-1
-        for i in range(len(s)-1, -1, -1):
-            if s[i] != ' ':
-                break;
-        s = s[:(i+1)]
-        i = s.rfind(' ')
-
-        return len(s) if i == -1 else len(s)-i-1
-            
-            
+        a = s.split(" ")
+        for i in range(len(a)-1, -1, -1):
+            if len(a[i]) != 0:
+                return len(a[i])
+        return 0
